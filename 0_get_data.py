7f9a7697cb7f4be2a9769c3ae2b3bb21e685bf23
Sven Balnojan <sven.balnojan@mercateo.com>
2019-11-04 21:58:58 +0100

    add a data get file

---
+++ 0_get_data.py
@@ -0,0 +1,5 @@
+import gluonnlp
+
+import gluonnlp as nlp
+train_dataset, test_dataset = [nlp.data.IMDB(root='data/imdb', segment=segment)
+                                for segment in ('train', 'test')]
